d7cd3c15a94fac0b95438a1597d27dc4b81fbbcb
lucasperea <lukeperea@gmail.com>
2016-09-23 15:59:13 -0700

    eod commit

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,8 +1,16 @@
 $(function(){
     $('#menu-btn').on("click",function(){
         $('#menu-toggle').toggle('slide', {direction: 'left'}, 200);
-         $('#wrapper').toggleClass('wrapper', 250);
-        $
+         $('#wrapper').toggleClass('wrapper', 200);
+         
     }); 
 });
+$(function(){
+    $('#help-sidebar').on("click",function(){
+        $('#help-menu').show('slide', {direction: 'right'}, 200);
+         
+         
+    }); 
+});
+
 
