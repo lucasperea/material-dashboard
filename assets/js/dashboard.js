f423feaa716b570cc719d9a3545472109f2492d5
lucasperea <lukeperea@gmail.com>
2016-09-23 09:58:13 -0700

    first commit to new repo

---
+++ assets/js/dashboard.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#menu-btn').on("click",function(){
+        $('#menu-toggle').toggle('slide', {direction: 'left'}, 200);
+         $('#wrapper').toggleClass('wrapper', 250);
+        $
+    }); 
+});
+
